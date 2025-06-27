e8546dba01549d974144a86beeb86f3faf3b581e
elizianeandreia <liziandreia@gmail.com>
2025-06-27 11:27:43 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,27 +1,21 @@
-document.addEventListener("mousemove", (evento: MouseEvent) => {
-  const pupilas = document.querySelectorAll<HTMLElement>(".pupila");
-
-  pupilas.forEach((pupila) => {
-    const caixaOlho = pupila.parentElement!.getBoundingClientRect();
-    const centroX = caixaOlho.left + caixaOlho.width / 2;
-    const centroY = caixaOlho.top + caixaOlho.height / 2;
-
-    const angulo = Math.atan2(evento.clientY - centroY, evento.clientX - centroX);
-    const deslocamentoX = Math.cos(angulo) * 10;
-    const deslocamentoY = Math.sin(angulo) * 10;
-
-    pupila.style.transform = `translate(${deslocamentoX}px, ${deslocamentoY}px)`;
-  });
+document.addEventListener("mousemove", function (evento) {
+    var pupilas = document.querySelectorAll(".pupila");
+    pupilas.forEach(function (pupila) {
+        var caixaOlho = pupila.parentElement.getBoundingClientRect();
+        var centroX = caixaOlho.left + caixaOlho.width / 2;
+        var centroY = caixaOlho.top + caixaOlho.height / 2;
+        var angulo = Math.atan2(evento.clientY - centroY, evento.clientX - centroX);
+        var deslocamentoX = Math.cos(angulo) * 10;
+        var deslocamentoY = Math.sin(angulo) * 10;
+        pupila.style.transform = "translate(".concat(deslocamentoX, "px, ").concat(deslocamentoY, "px)");
+    });
 });
-
-const formulario = document.querySelector(".form-login") as HTMLFormElement;
-const rostoNinja = document.querySelector(".rosto-ninja") as HTMLElement;
-const mensagem = document.getElementById("mensagemSucesso") as HTMLElement;
-
-formulario.addEventListener("submit", (evento) => {
-  evento.preventDefault();
-
-  rostoNinja.style.display = "none";
-  formulario.style.display = "none"; 
-  mensagem.classList.add("visivel");
+var formulario = document.querySelector(".form-login");
+var rostoNinja = document.querySelector(".rosto-ninja");
+var mensagem = document.getElementById("mensagemSucesso");
+formulario.addEventListener("submit", function (evento) {
+    evento.preventDefault();
+    rostoNinja.style.display = "none";
+    formulario.style.display = "none";
+    mensagem.classList.add("visivel");
 });
